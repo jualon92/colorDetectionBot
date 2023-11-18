7ab098e6c7b2efcfb34a4089267c180789332a33
Juan Ignacio Londra <londrajuan@gmail.com>
2023-11-18 03:46:49 -0300

    Update sendWppMessage.py to send WhatsApp message with additional information

--- sendWppMessage.py
+++ sendWppMessage.py
@@ -1,12 +1,16 @@
 # Importing the Required Library
 import pywhatkit
-import env
-# Defining the Phone Number and Message
-phone_number = env.phone
-message = "hola, este es un mensaje de prueba desde python"
+import env 
+import numpy as np
+ 
 
-# Sending the WhatsApp Message
-pywhatkit.sendwhatmsg_instantly(phone_number, message)
+def send_message(message):
+    # Defining the Phone Number and Message
+    phone_number = env.phone
+    message = "hola, soy un bot y estoy probando si puedo enviar mensajes de whatsapp desde python. Mensaje: " + message
 
-# Displaying a Success Message
-print("WhatsApp message sent!")
+    # Sending the WhatsApp Message
+    pywhatkit.sendwhatmsg_instantly(phone_number, message,13, 12, 32)
+    
+    # Displaying a Success Message
+    print("WhatsApp message sent!")
